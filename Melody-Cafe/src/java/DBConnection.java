cdaade6a15ff48ed6849ef45f89030cdbf16512b
Yeabsra Alebachew <yeabsraalebachew9@gmail.com>
2023-01-09 22:01:25 +0300

    first commit

---
+++ Melody-Cafe/src/java/DBConnection.java
@@ -0,0 +1,22 @@
+
+import java.sql.*;
+
+public class DBConnection {
+// JDBC driver name and database URL
+
+    static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
+    static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:xe";
+
+    //  Database credentials
+    static final String USER = "MELODY";
+    static final String PASS = "admin123";
+    Connection con = null;
+
+    public Connection connMethod() throws ClassNotFoundException, SQLException {
+        Class.forName(JDBC_DRIVER);
+      
+        con = DriverManager.getConnection(DB_URL, USER, PASS);
+
+        return con;
+    }
+}
