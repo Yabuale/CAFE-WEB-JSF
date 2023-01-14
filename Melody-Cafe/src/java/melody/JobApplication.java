0a73ffaa2f9bb2571c685bcbc9716e7c3d83b057
Yeabsra Alebachew <yeabsraalebachew9@gmail.com>
2023-01-14 22:20:36 +0300

    configured job application page

---
+++ Melody-Cafe/src/java/melody/JobApplication.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package melody;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author hp
+ */
+@ManagedBean
+public class JobApplication {
+    
+    private String name;
+    private String lname;
+    private String email;
+    private String phone;
+    private String adress;
+    private String exp;
+    
+    public JobApplication(){
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getExp() {
+        return exp;
+    }
+
+    public void setExp(String exp) {
+        this.exp = exp;
+    }
+    
+    public void insert(){
+         String sql="insert into APPICANTS (NAME,FNAME,EMAIL,PHONE,ADRESS,EXP) values ('"+name+"','"+lname+"','"+email+"','"+phone+"','"+adress+"','"+exp+"')";
+   
+        Statement s5;
+        try {
+            s5 = DBConnection.connMethod().createStatement();
+            s5.executeQuery(sql);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
